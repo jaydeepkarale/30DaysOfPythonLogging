0a759df9917250717848b0b0ee7b763c86b154bd
Jaydeep Karale <97970643+jaydeepkarale@users.noreply.github.com>
2022-04-24 16:08:28 +0530

    Added Day 9 content

---
+++ Day9Code/example.py
@@ -0,0 +1,50 @@
+# logging_example.py
+import logging
+import logging.handlers
+ 
+class TlsSMTPHandler(logging.handlers.SMTPHandler):
+    
+    def emit(self, record):
+        try:
+            import smtplib
+            import string # for tls add this line
+            try:
+                from email.utils import formatdate
+            except ImportError:
+                formatdate = self.date_time
+            port = self.mailport
+            if not port:
+                port = smtplib.SMTP_PORT
+            smtp = smtplib.SMTP(self.mailhost, port)
+            msg = self.format(record)
+            msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\nDate: %s\r\n\r\n%s" % (
+                            self.fromaddr,
+                            self.toaddrs,
+                            self.getSubject(record),
+                            formatdate(), msg)
+            if self.username:
+                smtp.ehlo() # for tls add this line
+                smtp.starttls() # for tls add this line
+                smtp.ehlo() # for tls add this line
+                smtp.login(self.username, self.password)
+            smtp.sendmail(self.fromaddr, self.toaddrs, msg)
+            smtp.quit()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+ 
+logger = logging.getLogger()
+ 
+# Adding the formatter
+format = logging.Formatter('%(asctime)s-%(levelname)s-%(funcName)s-%(msg)s')
+gm = TlsSMTPHandler(("smtp.gmail.com", 587), '<from_email>', ['<to_email>'], 'Error found!', ('username', 'pwd'))
+gm.setFormatter(format)
+gm.setLevel(logging.ERROR)
+ 
+logger.addHandler(gm)
+ 
+try:
+    1/0
+except:
+    logger.error('Divde By Zero')
